94e92f25c58c5f2c178c5c4cb3e0187aafbbe91c
Atul Tiwari <atltvri@gmail.com>
2020-11-27 23:07:08 +0530

    initial code

---
+++ mars.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(65 * 0.3783)
+	fmt.Print(" kgs, and I would be ")
+	fmt.Print(21 * 365 / 687)
+	fmt.Print(" years old.")
+}
