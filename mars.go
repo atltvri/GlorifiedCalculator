71a33780bd9773cdbff55bc10739f269041d6d4c
Atul Tiwari <atltvri@gmail.com>
2020-11-30 23:17:29 +0530

    new commit

--- mars.go
+++ mars.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Print("My weight on the surface of Mars is ")
-	fmt.Print(65 * 0.3783)
+	fmt.Print(65 * 0.3783) //0.3783 is a constant
 	fmt.Print(" kgs, and I would be ")
 	fmt.Print(21 * 365 / 687)
 	fmt.Print(" years old.")
